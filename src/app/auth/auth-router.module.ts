5295d17ba0ae0012268aa20b122b8148ab1e3737
MarGer <marinagernyak@gmail.com>
2019-09-13 00:46:31 +0300

    fix link router

--- src/app/auth/auth-router.module.ts
+++ src/app/auth/auth-router.module.ts
@@ -2,8 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ForgotEmailComponent } from './forgot-email/forgot-email.component'
 import {RegistrationComponent} from '../auth/registration/registration.component'
+import {LoginInlineComponent} from '../auth/login-inline/login-inline.component'
+
+
 
 const routes: Routes = [
+  { path: 'signin', component: LoginInlineComponent },
   { path: 'registration', component: RegistrationComponent },
   { path: 'forgot-password', component: ForgotEmailComponent },
   // { path: 'update-password', component: UpdatePasswordComponent },
