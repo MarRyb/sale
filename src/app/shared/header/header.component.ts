1dd4e2e33c39702ad99ae593c603112c20fba5a3
MarGer <marinagernyak@gmail.com>
2019-09-18 19:30:12 +0300

    fix logout service

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CurrentUserService } from '../../core/services/current-user.service';
+import { from } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor(
+    public currentUserService: CurrentUserService
   ) { }
 
   ngOnInit() {
