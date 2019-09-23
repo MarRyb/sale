a6e38dd1242352de0bf73337df37ce888e7a2837
MarGer <marinagernyak@gmail.com>
2019-09-23 11:11:06 +0300

    fix build error

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,13 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { CurrentUserService } from '../../core/services/current-user.service';
-import { from } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
 export class HeaderComponent implements OnInit {
-
+  public showSettings = false;
   constructor(
     public currentUserService: CurrentUserService
   ) { }
