89fcff00ec3adbef6e7e2afb87ca426865ae8960
MarGer <marinagernyak@gmail.com>
2019-10-04 14:18:11 +0300

    fix url

--- src/app/categories/categories-routing.module.ts
+++ src/app/categories/categories-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  { path: '',  component: CategoriesShowComponent},
   { path: ':category',  component: CategoriesShowComponent}
 ];
 
