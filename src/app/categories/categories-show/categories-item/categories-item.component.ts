df3f0181e816d03760397ea33487616ddf1f141a
Artem Dubovikov <artemdubovikov@gmail.com>
2019-10-08 00:48:23 +0300

    add interface post

--- src/app/categories/categories-show/categories-item/categories-item.component.ts
+++ src/app/categories/categories-show/categories-item/categories-item.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Post } from 'src/app/core/interfaces/post';
 
 @Component({
-  selector: 'app-categories-item',
-  templateUrl: './categories-item.component.html',
-  styleUrls: ['./categories-item.component.sass']
+    selector: 'app-categories-item',
+    templateUrl: './categories-item.component.html',
+    styleUrls: ['./categories-item.component.sass']
 })
 export class CategoriesItemComponent implements OnInit {
-  @Input() posts: [];
-  constructor() {
-  }
+    @Input() posts: Post[];
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
---
+++ src/app/core/interfaces/post.ts
@@ -0,0 +1,97 @@
+export interface Type {
+    id: number;
+    type: string;
+}
+
+export interface PaymentType {
+    id: number;
+    type: Type;
+    info: string;
+}
+
+export interface Country {
+    id: number;
+    name: string;
+}
+
+export interface Region {
+    id: number;
+    name: string;
+    country: Country;
+}
+
+export interface City {
+    id: number;
+    name: string;
+    region: Region;
+}
+
+export interface DeliveryAddress {
+    id: number;
+    city: City;
+    address: string;
+    price: number;
+    type: string;
+}
+
+export interface User {
+    id: number;
+    user_id: number;
+    payment_types: PaymentType[];
+    delivery_addresses: DeliveryAddress[];
+    count_bonus: number;
+}
+
+export interface CustomField {
+    id: number;
+    name: string;
+    type: string;
+}
+
+export interface Category {
+    id: number;
+    name: string;
+    children: Category[];
+    custom_fields: CustomField[];
+    slug: string;
+}
+
+export interface Currency {
+    id: number;
+    name: string;
+    symbol: string;
+}
+
+
+export interface PostCustomField {
+    id: number;
+    value: string;
+    custom_field: CustomField;
+}
+
+export interface HistoryPost {
+    id: number;
+    previous_post: Post;
+    created: Date;
+    updated: Date;
+}
+
+export interface Post {
+    id: number;
+    user: User;
+    title: string;
+    content: string;
+    price: number;
+    category: Category;
+    type: Type;
+    currency: Currency;
+    data: any[];
+    post_custom_fields: PostCustomField[];
+    status: string;
+    history_previous_post: Post[];
+    history_post: HistoryPost[];
+    count: number;
+    created: Date;
+    updated: Date;
+}
+
