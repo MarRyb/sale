78529e75c2844b7d9507c19f722b088de102b4af
Artem <artemdubovikov@gmail.com>
2019-11-01 11:07:39 +0300

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   chatURL: 'http://test3.vpotoke.com:8890/',
   apiUrl: 'http://test4.vpotoke.com/',
   openApiURL: 'http://test2.vpotoke.com/open_api/v1/',
-  URL: 'http://test2.vpotoke.com/',
+  URL: 'http://test4.vpotoke.com/',
   tokenURL: 'http://test2.vpotoke.com/oauth/v2/token',
   client_id: '1_60xq1vhsnqosgk40w8ssg8k4kskwc8sok0c40ogscko0o4gscs',
   client_secret: '3m68hi3izi4gssw8kc880ssco8woc0w84w0gg4kwkg8wc8okgo',
