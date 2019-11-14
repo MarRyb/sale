5ca0c8e662c4ee6d21c104c009551c517637a39d
MarGer <marinagernyak@gmail.com>
2019-11-14 16:40:52 +0200

    fix interceptor

--- src/app/core/interceptors/httpconfig.interceptor.ts
+++ src/app/core/interceptors/httpconfig.interceptor.ts
@@ -13,8 +13,6 @@ export class HttpConfigInterceptor implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const headersConfig = {
-    //   'Content-Type': 'application/json',
-    //   Accept: 'application/json',
       authorization: ''
     };
 
