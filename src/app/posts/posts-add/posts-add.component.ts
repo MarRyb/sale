0b0fc2bc8d0da337868d5968ebb4d3032eba488a
Artem Dubovikov <artemdubovikov@gmail.com>
2019-11-20 22:56:53 +0300

    add observable

--- src/app/posts/posts-add/posts-add.component.ts
+++ src/app/posts/posts-add/posts-add.component.ts
@@ -1,159 +1,195 @@
 import { PostsService } from './../../core/services/post.service';
 import { ITooltip } from './../../core/interfaces/tooltip.interface';
-import { Component, OnInit, TemplateRef, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, TemplateRef, Output, EventEmitter, OnDestroy } from '@angular/core';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
-import {FormGroup, FormControl, Validators} from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { FileError, NgxfUploaderService, UploadEvent, UploadStatus } from 'ngxf-uploader';
-import { Observable, from } from 'rxjs';
-import { concatMap } from 'rxjs/operators';
+import { Observable, from, ReplaySubject } from 'rxjs';
+import { concatMap, takeUntil, switchMap, tap, catchError } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-posts-add',
-  templateUrl: './posts-add.component.html',
-  styleUrls: ['./posts-add.component.scss']
+    selector: 'app-posts-add',
+    templateUrl: './posts-add.component.html',
+    styleUrls: ['./posts-add.component.scss']
 })
-export class PostsAddComponent implements OnInit {
-  public isFocus: boolean = false;
-  public settingsTooltipInfo: ITooltip;
-  public settingsTooltipWarning: ITooltip;
-  public selectedCategories = [];
-  public isShowSelectRubrics = true;
-  modalRef: BsModalRef;
-  public form: FormGroup;
-  public fields: any[];
-  public photoItem: any = {};
-  public postForm: FormGroup;
-  isUploading: boolean = true;
-  progress: number = 0;
-  public photos: any[] = [
-    {uploadStatus: {}}, {uploadStatus: {}}, {uploadStatus: {}}, {uploadStatus: {}}, {uploadStatus: {}}, {uploadStatus: {}}, {uploadStatus: {}}
-  ];
+export class PostsAddComponent implements OnInit, OnDestroy {
+    public isFocus: boolean = false;
+    public settingsTooltipInfo: ITooltip;
+    public settingsTooltipWarning: ITooltip;
+    public selectedCategories = [];
+    public isShowSelectRubrics = true;
+    modalRef: BsModalRef;
+    public form: FormGroup;
+    public fields: any[];
+    public photoItem: any = {};
+    public postForm: FormGroup;
+    isUploading: boolean = true;
+    progress: number = 0;
+    public photos: any[] = [
+        { uploadStatus: {} }, { uploadStatus: {} }, { uploadStatus: {} }, { uploadStatus: {} }, { uploadStatus: {} }, { uploadStatus: {} }, { uploadStatus: {} }
+    ];
 
-  @Output() onClose: EventEmitter<any> = new EventEmitter();
+    @Output() onClose: EventEmitter<any> = new EventEmitter();
 
-  constructor(
-    private modalService: BsModalService,
-    public postService: PostsService,
-    private Upload: NgxfUploaderService
-  ) {
-    this.settingsTooltipInfo = {
-      imgUrl: 'assets/img/icon Инфо.png',
-      placement: 'right',
-      contentHtml: `
+    private ngOnDestroy$: ReplaySubject<null> = new ReplaySubject<null>();
+    loadingPostCreate: boolean;
+
+    constructor(
+        private modalService: BsModalService,
+        public postService: PostsService,
+        private Upload: NgxfUploaderService
+    ) {
+        this.settingsTooltipInfo = {
+            imgUrl: 'assets/img/icon Инфо.png',
+            placement: 'right',
+            contentHtml: `
         <div>Введите наименование товара или услуги.</div>
         <br>
         <div>Чем точнее будет заголовок, тем больше вероятность что на Ваше обьявление отреагируют</div>
       `,
-      click: ''
-    };
-    this.settingsTooltipWarning = {
-      imgUrl: 'assets/img/warning.png',
-      placement: 'right',
-      contentHtml: `
+            click: ''
+        };
+        this.settingsTooltipWarning = {
+            imgUrl: 'assets/img/warning.png',
+            placement: 'right',
+            contentHtml: `
         <div class="tooltip-text-red">Введите описание.</div>
         <br>
         <div class="tooltip-text-red">Описание должно быть не мение 20 символов.</div>
       `,
-      click: ''
-    };
-    this.form = new FormGroup({
-      fields: new FormControl(this.fields)
-    });
-  }
-
-
-// START
-
-
-  uploadFile(file: File | FileError, index: number): void {
-    var photo = this.photos[index];
-    photo.uploadStatus.isUploading = true;
-    if (!(file instanceof File)) {
-      photo.uploadStatus.isUploading = false;
-      return;
+            click: ''
+        };
+        this.form = new FormGroup({
+            fields: new FormControl(this.fields)
+        });
     }
-    this.Upload.upload({
-      url: 'http://test4.vpotoke.com/api/v1/posts/files',
-      filesKey: 'path', // Option
-      files: file,
-      process: true
-    }).subscribe(
-      (event: UploadEvent) => {
-        photo.uploadStatus.progress = event.percent;
-        if (event.status === UploadStatus.Completed) {
-          photo.file = event.data;
+
+
+    // START
+
+
+    uploadFile(file: File | FileError, index: number): void {
+        const photo = this.photos[index];
+        photo.uploadStatus.isUploading = true;
+        if (!(file instanceof File)) {
+            photo.uploadStatus.isUploading = false;
+            return;
         }
-      },
-      (err) => {
-      },
-      () => {
-        photo.uploadStatus.isUploading = false;
-      });
-  }
-
-// END
-
-
-  openModal(template: TemplateRef<any>) {
-    this.modalRef = this.modalService.show(template, {animated: true});
-  }
-  focusInput() {
-    this.isFocus = true;
-    this.settingsTooltipInfo.click = 'click';
-  }
-
-  ngOnInit() {
-    this.postForm = new FormGroup({
-      title: new FormControl(null, [Validators.required]),
-      content: new FormControl(null, [Validators.required]),
-      category: new FormControl(null),
-      type: new FormControl(null),
-      price: new FormControl(null),
-      currency: new FormControl(null),
-      // bargain: new FormGroup({ barg: new FormControl('') }),
-      postCustomFields: new FormControl(null)
-    });
-  }
-
-
-  selectedCategory(item) {
-    this.selectedCategories = item.array;
-    if (item.hideModal === true) {
-      this.modalRef.hide();
-      this.isShowSelectRubrics = false;
+        this.Upload.upload({
+            url: 'http://test4.vpotoke.com/api/v1/posts/files',
+            filesKey: 'path', // Option
+            files: file,
+            process: true
+        }).subscribe(
+            (event: UploadEvent) => {
+                photo.uploadStatus.progress = event.percent;
+                if (event.status === UploadStatus.Completed) {
+                    photo.file = event.data;
+                }
+            },
+            (err) => {
+            },
+            () => {
+                photo.uploadStatus.isUploading = false;
+            });
     }
-    this.postForm.controls.category.setValue(item.itemCategory.id);
-    this.fields = item.itemCategory.customFields;
-  }
+
+    // END
 
 
-  getFields() {
-    return this.fields;
-  }
-
-  onSubmit() {
-    this.postService.new(this.postForm.value).subscribe(data => {
-      this.onClose.emit();
-      this.attachPhotosToPost(data.id).subscribe(data => {
-        console.log(data);
-      });
-    }, (err) => {
-      this.modalRef.hide();
-    });
-  }
-
-  attachPhotosToPost(postId: number): Observable<any> {
-    const photos = this.photos.filter(i => i.file && i.file.id);
-    return from(photos).pipe(
-        concatMap(file => this.postService.attachPostFile(postId, file.file.id)));
-  }
-
-  getCustomFields(data: any) {
-    const postCustomFields = [];
-    for (const key in data) {
-      postCustomFields.push({ value: data[key], customField: key });
+    openModal(template: TemplateRef<any>) {
+        this.modalRef = this.modalService.show(template, { animated: true });
     }
-    this.postForm.controls.postCustomFields.setValue(postCustomFields);
-  }
+    focusInput() {
+        this.isFocus = true;
+        this.settingsTooltipInfo.click = 'click';
+    }
+
+    ngOnInit() {
+        this.postForm = new FormGroup({
+            title: new FormControl(null, [Validators.required]),
+            content: new FormControl(null, [Validators.required]),
+            category: new FormControl(null),
+            type: new FormControl(null),
+            price: new FormControl(null),
+            currency: new FormControl(null),
+            // bargain: new FormGroup({ barg: new FormControl('') }),
+            postCustomFields: new FormControl(null)
+        });
+    }
+
+
+    selectedCategory(item) {
+        this.selectedCategories = item.array;
+        if (item.hideModal === true) {
+            this.modalRef.hide();
+            this.isShowSelectRubrics = false;
+        }
+        this.postForm.controls.category.setValue(item.itemCategory.id);
+        this.fields = item.itemCategory.customFields;
+    }
+
+
+    getFields() {
+        return this.fields;
+    }
+
+    onSubmit() {
+        // this.postService.new(this.postForm.value).subscribe(data => {
+        //     this.onClose.emit();
+        //     this.attachPhotosToPost(data.id).subscribe(data => {
+        //         console.log(data);
+        //     });
+        // }, (err) => {
+        //     this.modalRef.hide();
+        // });
+
+        const payload = this.postForm.getRawValue();
+        console.log(payload);
+        // можно тут при сабмите сразу
+        this.loadingPostCreate = true;
+        this.createPost(payload);
+    }
+
+    attachPhotosToPost(postId: number): Observable<any> {
+        const photos = this.photos.filter(i => i.file && i.file.id);
+        return from(photos).pipe(
+            concatMap(file => this.postService.attachPostFile(postId, file.file.id)));
+    }
+
+    getCustomFields(data: any) {
+        const postCustomFields = [];
+        // tslint:disable-next-line: forin
+        for (const key in data) {
+            postCustomFields.push({ value: data[key], customField: key });
+        }
+        this.postForm.controls.postCustomFields.setValue(postCustomFields);
+    }
+
+    createPost(data: any) {
+        // цельный поток данных. Можно обработать err еще 
+        // catchError()
+        this.postService.new(this.postForm.value)
+            .pipe(
+                // можно тут поставить начало загрузки поста
+                // или начало загрузки файлов
+                // tap(() => {
+                //     this.loadingPostCreate = true;
+                // }),
+                switchMap(post => this.attachPhotosToPost(post.id)),
+                tap(() => this.loadingPostCreate = false),
+                takeUntil(this.ngOnDestroy$) // должен быть последним, отписка!
+            )
+            .subscribe(endPost => {
+                console.log(endPost);
+                this.onClose.emit(/* endPost */);
+                this.modalRef.hide();
+            });
+    }
+
+    ngOnDestroy() {
+        this.ngOnDestroy$.next(null);
+        this.ngOnDestroy$.complete();
+    }
 
 }
